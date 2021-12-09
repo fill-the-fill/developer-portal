ca02db4bf8d23e0481eece8fba5f0550f58df468
fill-the-fill <fill.ickevics@gmail.com>
2021-12-09 12:42:58 +0200

    Add error handling in CIP fetching

--- scripts/cip-script.js
+++ scripts/cip-script.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const path = require("path")
-const axios = require('axios');
+const axios = require('axios')
 
 const gitBaseUrl = 'https://github.com/cardano-foundation/CIPs/tree/master'
-const rawBaseUrl = 'https://raw.githubusercontent.com/cardano-foundation/CIPs/master';
+const rawBaseUrl = 'https://raw.githubusercontent.com/cardano-foundation/CIPs/master'
 const apiBaseUrl = "https://api.github.com/repos/cardano-foundation/CIPs/git/trees/master"
-const cipDocsPath = "/docs/governance/cardano-improvement-proposals/";
-const ___dirname = path.resolve(path.dirname(''));
+const cipDocsPath = "/docs/governance/cardano-improvement-proposals/"
+const ___dirname = path.resolve(path.dirname(''))
 
 // Array to store CIP folders 
-let folders = [];
+let folders = []
 
 // Fetching CIP folder names, then fetching README raw files
 let fetchFolderNames = async () => {
@@ -24,8 +24,8 @@ let fetchFolderNames = async () => {
         folders.splice(0, folders.length, ...cipFolderNames)
         // Fetching README's and locally downloading data
         folders.forEach(async (folder) => {
-            const content = await fetchFileContent(folder);
-        });
+            const content = await fetchFileContent(folder)
+        })
     }).catch(error => {
         console.log(error)
     })
@@ -34,35 +34,35 @@ let fetchFolderNames = async () => {
 // Fetching README.md files in every CIP folder and downloading it locally
 let fetchFileContent = async (fileName) => {
     try {
-        const response = await axios.get(`${rawBaseUrl}` + "/" + `${fileName}` + "/README.md");
+        const response = await axios.get(`${rawBaseUrl}` + "/" + `${fileName}` + "/README.md")
       
         // Stripping HTML && Removing first 3 `---` && Splitting
-        const stripHTML = response.data.slice(3).replace(/<[^>]+>/g, '').split("\n");
+        const stripHTML = response.data.slice(3).replace(/<[^>]+>/g, '').split("\n")
         
         // Adding sidebar_label tag to each document and stripping HTML
-        const sideBarLabelKey = "--- \nsidebar_label: " + fileName;
+        const sideBarLabelKey = "--- \nsidebar_label: " + fileName
 
         // Stripping `\` symbol from headlines 
         const cleanerStringResult =
             stripHTML.map(s => s.includes("####" && "###" && "##" && "#") ? s.replace(/\\/g, '') : s
                 // Fixing parent links to CIPs
                 && s.includes("](../") ? s.replace("../", "./") : s
-            );
+            )
         // Rewritting relative URLs to absolute URLs 
         const fileRedirectStringResult =
             cleanerStringResult.map(s => s.includes("](./") ? s.replace("](./", "./", gitBaseUrl + '/' + fileName + '/') : s
                 // Enforcing H2 headlines (Docusaurus doesn't like that)
                 && s.includes("# Abstract") && !s.includes("## Abstract") ? s.replace("#", "##") : s
-            ).join("\n");
+            ).join("\n")
 
         // Downloading files locally
         fs.writeFile(___dirname + cipDocsPath + fileName + ".md", sideBarLabelKey + fileRedirectStringResult, (err) => {
             if (err)
-                console("Oops, there has been a problem with downloading " + fileName)
+                console("Oops, there has been a problem with downloading " + fileName, err)
             else {
-                console.log("File " + fileName + " has been added to " + cipDocsPath + fileName);
+                console.log("File " + fileName + " has been added to " + cipDocsPath + fileName)
             }
-        });
+        })
         // TO DO: Downloading Images for CIPs
     }
     catch (error) {
@@ -72,8 +72,8 @@ let fetchFileContent = async (fileName) => {
 
 //Calling script
 let main = async () => {
-    console.log("CIP Content Downloading...");
-    const data = await fetchFolderNames();
+    console.log("CIP Content Downloading...")
+    const data = await fetchFolderNames()
 }
 
-main();
+main()
