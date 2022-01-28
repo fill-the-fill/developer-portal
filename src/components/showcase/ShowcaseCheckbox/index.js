69110b2e48129a3c6f80412206cf38d64777f268
fill-the-fill <fill.ickevics@gmail.com>
2022-01-28 14:51:57 +0200

    Add search bar into showcase page & Modify filtering design

--- src/components/showcase/ShowcaseCheckbox/index.js
+++ src/components/showcase/ShowcaseCheckbox/index.js
@@ -5,32 +5,32 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from "react";
-import clsx from "clsx";
-
-import styles from "./styles.module.css";
-
-function ShowcaseCheckbox({
-  className,
-  name,
-  label,
-  onChange,
-  checked,
-  ...props
-}) {
-  const id = `showcase_checkbox_id_${name};`;
-  return (
-    <div className={clsx(props.className, styles.checkboxContainer)} {...props}>
-      <input
-        type="checkbox"
-        id={id}
-        name={name}
-        onChange={onChange}
-        checked={checked}
-      />
-      <label htmlFor={id}>{label}</label>
-    </div>
-  );
-}
-
-export default ShowcaseCheckbox;
+ import React from "react";
+ import clsx from "clsx";
+ 
+ import styles from "./styles.module.css";
+ 
+ function ShowcaseCheckbox({
+   className,
+   name,
+   label,
+   onChange,
+   checked,
+   ...props
+ }) {
+   const id = `showcase_checkbox_id_${name};`;
+   return (
+     <div className={clsx(props.className, styles.checkboxContainer)} {...props}>
+       <input
+         type="checkbox"
+         id={id}
+         name={name}
+         onChange={onChange}
+         checked={checked}
+       />
+       <label htmlFor={id}>{label}</label>
+     </div>
+   );
+ }
+ 
+ export default ShowcaseCheckbox;
